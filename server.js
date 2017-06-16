c6492c26b53271cd3856bc81c97b6316fbebaf53
xxabhi04 <anandabhinav16@gmail.com>
2017-06-16 16:38:43 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express=require('express');
 var app=express();
 
-var response={};
-
 app.use(function (req,res) {
     console.log(req.headers);
+    var response={}
     response.ipaddress=req.ip.split(':')[req.ip.split(':').length-1];
     response.language=req.get("accept-language").split(',')[0];
     response.software=req.get("user-agent").split('(')[1].split(')')[0];
