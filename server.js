a2672901089d32d1ec992de7681e634e1ccbdbae
xxabhi04 <anandabhinav16@gmail.com>
2017-06-16 17:25:10 +0530

    changes for obtaining ip address

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ var express=require('express');
 var app=express();
 
 app.use(function (req,res) {
-    console.log(req.headers);
-    var response={}
-    response.ipaddress=req.ip.split(':')[req.ip.split(':').length-1];
+    var response={};
+
+    if(req.headers['x-forwarded-for']!=undefined)
+        response.ipaddress=req.headers['x-forwarded-for'].split(',')[0];
+
+    else response.ipaddress=req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+
     response.language=req.get("accept-language").split(',')[0];
     response.software=req.get("user-agent").split('(')[1].split(')')[0];
     res.send(JSON.stringify(response));
-
 });
-
-
 
 app.listen(8080);
