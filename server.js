cadb14cea262e16776e331fd84b13bf0e682231c
xxabhi04 <anandabhinav16@gmail.com>
2017-06-16 16:37:46 +0530

    minor changes

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ var app=express();
 
 var response={};
 
-
-
 app.use(function (req,res) {
     console.log(req.headers);
-    response.ip=req.ip;
-    response.language=req.headers["accept-language"].split(',')[0];
-    response.software=req.headers["user-agent"].split('(')[1].split(')')[0];
+    response.ipaddress=req.ip.split(':')[req.ip.split(':').length-1];
+    response.language=req.get("accept-language").split(',')[0];
+    response.software=req.get("user-agent").split('(')[1].split(')')[0];
     res.send(JSON.stringify(response));
 
 });
